44febbea908a8338b176abd8243505c9e0e5640e
AlexiusC2 <alexius.christhoper@ui.ac.id>
2025-09-09 14:09:42 +0700

    Fix link

--- mustard_sports/settings.py
+++ mustard_sports/settings.py
@@ -29,7 +29,8 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alexius-christhoper-mustardsports.pbp.cs.ui.ac.id"]
+
 
 
 # Application definition
