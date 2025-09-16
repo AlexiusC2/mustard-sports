211d887e4c327ad1ba31f20d5b0815e988c4f541
AlexiusC2 <alexius.christhoper@ui.ac.id>
2025-09-17 03:26:05 +0700

    tugas 3 readme

--- main/urls.py
+++ main/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product/', add_product, name='add_product'),
     path('product/<str:id>/', show_product, name='show_product'),
-    path('product/xml/', show_xml, name='show_xml'),
-    path('product/json/', show_json, name='show_json'),
-    path('product/xml/<int:product_id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('product/json/<int:product_id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<int:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:product_id>/', show_json_by_id, name='show_json_by_id'),
 ]
